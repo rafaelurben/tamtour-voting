1027100d199e3150ac32c18f0f9e882afbf30132
rafaelurben <github@rafaelurben.ch>
2025-07-17 16:47:22 +0200

    feat: Setup backend with Spring Boot 3.5.3, gradle and spotless

---
+++ voting-backend/src/main/java/ch/rafaelurben/tamtour/voting/VotingBackendApplication.java
@@ -0,0 +1,12 @@
+package ch.rafaelurben.tamtour.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VotingBackendApplication.class, args);
+  }
+}
---
+++ voting-backend/src/test/java/ch/rafaelurben/tamtour/voting/VotingBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package ch.rafaelurben.tamtour.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingBackendApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
