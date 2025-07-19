94b2e3dac2b0165571a52d9fe2fa79a26881dd98
rafaelurben <github@rafaelurben.ch>
2025-07-19 04:42:11 +0200

    feat: Add basic OAuth config

---
+++ voting-backend/src/main/java/ch/rafaelurben/tamtour/voting/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package ch.rafaelurben.tamtour.voting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+        .oauth2Login(
+            oauth2 ->
+                oauth2
+                    .loginPage("/api/oauth2/authorization/google")
+                    .defaultSuccessUrl("/app", true)
+                    .authorizationEndpoint(authz -> authz.baseUri("/api/oauth2/authorization"))
+                    .redirectionEndpoint(redir -> redir.baseUri("/api/login/oauth2/code/*")))
+        .logout(
+            logout ->
+                logout
+                    .logoutUrl("/api/logout")
+                    .logoutSuccessUrl("/")
+                    .deleteCookies("JSESSIONID"));
+    return http.build();
+  }
+}
