ea4aa16393cbb6dbc0470677220582d9e6754f3e
Rafael Urben <github@rafaelurben.ch>
2025-07-25 18:26:05 +0200

    feat: Add basic page components for login, register and home.

--- voting-frontend/src/app/app.routes.ts
+++ voting-frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login-page/login-page';
+import { RegisterPage } from './pages/register-page/register-page';
+import { HomePage } from './pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '/',
+    component: LoginPage,
+  },
+  {
+    path: '/register',
+    component: RegisterPage,
+  },
+  {
+    path: '/app',
+    component: HomePage,
+  },
+];
