6fc35da596461a106f191ea5aa0c809dbe5b0190
Rafael Urben <github@rafaelurben.ch>
2025-07-25 23:24:37 +0200

    feat(frontend): Add simple form to register page

--- voting-frontend/src/app/pages/register-page/register-page.ts
+++ voting-frontend/src/app/pages/register-page/register-page.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../api/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
-export class RegisterPage {}
+export class RegisterPage {
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  protected nameForm = this.formBuilder.nonNullable.group({
+    firstName: [this.authService.user()?.firstName || '', Validators.required],
+    lastName: [this.authService.user()?.lastName || '', Validators.required],
+  });
+
+  protected onSubmit() {
+    if (this.nameForm.invalid) return;
+    const { firstName, lastName } = this.nameForm.getRawValue();
+    this.authService.updateMe({ firstName, lastName }).subscribe({
+      next: () => {
+        this.router.navigate(['/app']);
+      },
+    });
+  }
+}
