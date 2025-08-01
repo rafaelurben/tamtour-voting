2c404c30970c60a11b2fc6eb671c686220a99ab8
rafaelurben <github@rafaelurben.ch>
2025-08-01 18:30:00 +0200

    style: Update header, add avatar placeholder

--- voting-frontend/src/app/components/header/header.ts
+++ voting-frontend/src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { AuthService } from '../../api/auth.service';
 import { RouterLink } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
@@ -10,5 +10,24 @@ import { NgOptimizedImage } from '@angular/common';
   styleUrl: './header.css',
 })
 export class Header {
+  private readonly DEFAULT_PROFILE_IMAGE = '/avatar-placeholder.svg';
+
   protected readonly authService = inject(AuthService);
+  protected readonly profileImageUrl = signal(this.DEFAULT_PROFILE_IMAGE);
+
+  constructor() {
+    effect(() => {
+      const user = this.authService.user();
+      if (user?.pictureLink) {
+        this.profileImageUrl.set(user.pictureLink);
+      } else {
+        this.profileImageUrl.set(this.DEFAULT_PROFILE_IMAGE);
+      }
+    });
+  }
+
+  onProfileImageError(event: Event) {
+    const target = event.target as HTMLImageElement;
+    target.src = this.DEFAULT_PROFILE_IMAGE;
+  }
 }
