3b229d34c2279970ecfd72a57f83734816af777b
rafaelurben <github@rafaelurben.ch>
2025-08-24 18:34:49 +0200

    feat(frontend): Add users admin page

--- voting-frontend/src/app/pages/admin-page/subpages/user-admin-page/user-admin-page.ts
+++ voting-frontend/src/app/pages/admin-page/subpages/user-admin-page/user-admin-page.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { VotingUserDto } from '../../../../dto/voting-user.dto';
+import { Observable } from 'rxjs';
+import { AdminVotingUsersApi } from '../../../../api/admin/admin-voting-users.api';
+import { Spinner } from '../../../../components/spinner/spinner';
+import {
+  AsyncPipe,
+  DatePipe,
+  NgOptimizedImage,
+  NgStyle,
+} from '@angular/common';
+import { Button } from '../../../../components/button/button';
 
 @Component({
   selector: 'app-user-admin-page',
-  imports: [],
+  imports: [Spinner, AsyncPipe, DatePipe, NgOptimizedImage, NgStyle, Button],
   templateUrl: './user-admin-page.html',
   styleUrl: './user-admin-page.css',
 })
-export class UserAdminPage {}
+export class UserAdminPage {
+  protected users$!: Observable<VotingUserDto[]>;
+  protected blockButtonLoadingIds: number[] = [];
+
+  private readonly votingUsersApi = inject(AdminVotingUsersApi);
+
+  constructor() {
+    this.users$ = this.votingUsersApi.getUsers();
+  }
+
+  protected blockUser(userId: number): void {
+    this.blockButtonLoadingIds.push(userId);
+    this.votingUsersApi.blockUser(userId).subscribe(() => {
+      this.users$ = this.votingUsersApi.getUsers();
+      this.blockButtonLoadingIds.splice(
+        this.blockButtonLoadingIds.indexOf(userId),
+        1
+      );
+    });
+  }
+
+  protected unblockUser(userId: number): void {
+    this.blockButtonLoadingIds.push(userId);
+    this.votingUsersApi.unblockUser(userId).subscribe(() => {
+      this.users$ = this.votingUsersApi.getUsers();
+      this.blockButtonLoadingIds.splice(
+        this.blockButtonLoadingIds.indexOf(userId),
+        1
+      );
+    });
+  }
+}
